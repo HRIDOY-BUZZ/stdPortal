e6eb93a69b65d16d6ca03fd56910f6655621a4af
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2022-09-05 12:14:41 +0600

    set result done

---
+++ admin/backend/set_result.php
@@ -0,0 +1,37 @@
+<?php
+    $type   = "";
+    $msg    = "";
+
+    $sql    =   "SELECT * FROM `Courses` WHERE 1";
+    $query  =   mysqli_query($conn, $sql);
+    $crs    =   mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+    $sql    =   "SELECT * FROM `Students` WHERE 1";
+    $query  =   mysqli_query($conn, $sql);
+    $std    =   mysqli_fetch_all($query, MYSQLI_ASSOC);
+    if($_POST['submit'])
+    {
+        // dd($_POST);
+        $course     = $_POST['course'];
+        // $semester   = $_POST['semester'];
+        // $year       = $_POST['year'];
+        $student    = $_POST['student'];
+        $ocg        = $_POST['o_cgpa'];
+        $hcg        = $_POST['h_cgpa'];
+        
+
+            $sql = "INSERT INTO `Results`(`course_id`, `std_id`, `grade`, `highest`) 
+                    VALUES ($course, $student, $ocg, $hcg)";
+            
+            if(!mysqli_query($conn, $sql))
+            {
+                $type   = "error";
+                $msg    = "Database Error! Try Again...";
+            }
+            else
+            {
+                $type   = "success";
+                $msg    = "Result Added Successfully!!";
+            }
+    }
+?>
--- admin/set_result.php
+++ admin/set_result.php
@@ -1,61 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include "admin_head.php";?>
+<?php 
+    include "admin_head.php";
+    include "../backend/functions.php";
+    include "backend/set_result.php";
+?>
 <body class="">
 <?php include "../header_nav.php";?>
     <section class="header-container">
         <?php include "admin_sidebar.php";?>
     </section>
-    <section id="home-container"  class="home-container active-2"">
+    <section id="home-container"  class="home-container active-2">
         <div class="px-4 py-5 mt-4">
             <h1 class="common-background-2 p-2 text-center">Set Result</h1>
-
-                <form class="w-75 mx-auto" action="" method="">
-                    <div class="form-floating mb-3">
-                        <select class="form-select py-2" id="semester" name="semester" required>
-                            <option selected>Semester</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
-
+            <div class="container alerts">
+                <?php
+                    if($msg != "")
+                    {
+                        echo message($type, $msg);
+                    }
+                ?>
+            </div>
+                <form class="w-75 mx-auto" action="" method="post">
                     <div class="form-floating mb-3">
                         <select class="form-select py-2" id="course" name="course" required>
-                            <option selected>course</option>
-                            <option value="1">CSE-105</option>
-                            <option value="2">Gen-202</option>
-                            <option value="3">Cse-200</option>
+                            <option selected>- - Select Course</option>
+                            <?php
+                                if(!empty($crs))
+                                {
+                                    foreach($crs as $c)
+                                    {
+                                        echo '<option value="'.$c['id'].'">'.$c['course_name'].'</option>';
+                                    }
+                                }
+                            ?>
+                        </select>
+                    </div>
+
+                    <!-- <div class="form-floating mb-3">
+                        <select class="form-select py-2" id="semester" name="semester" required>
+                            <option selected>- - Select Semester</option>
+                            <option value="1">Spring</option>
+                            <option value="2">Summer</option>
+                            <option value="3">Fall</option>
                         </select>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" name="credit" id="credit" placeholder=" " required>
-                        <label for="credit">Credit</label>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" name="year" id="year" placeholder=" " required>
+                        <input type="number" class="form-control" name="year" id="year" min="2000" max="3000" placeholder=" " required>
                         <label for="year">Year</label>
+                    </div> -->
+
+                    <div class="form-floating mb-3">
+                        <select class="form-select py-2" id="student" name="student" required>
+                            <option selected>- - Select Student</option>
+                            <?php
+                                if(!empty($std))
+                                {
+                                    foreach($std as $s)
+                                    {
+                                        echo '<option value="'.$s['std_id'].'">'.$s['std_id'].'. '.$s['name'].'</option>';
+                                    }
+                                }
+                            ?>
+                        </select>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" name="student_name" id="student_name" placeholder=" ">
-                        <label for="student_name">Student Name</label>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" name="student_id" id="student_id" placeholder=" " required>
-                        <label for="student_id">Student Id</label>
-                    </div>
-
-                    <div class="form-floating mb-3">
-                        <input type="number" class="form-control" name="o_cgpa" id="o_cgpa" placeholder=" " required>
+                        <input type="number" step="0.01" class="form-control" name="o_cgpa" id="o_cgpa" placeholder=" " required>
                         <label for="o_cgpa">Obtained CGPA</label>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="number" class="form-control" name="h_cgpa" id="h_cgpa" placeholder=" " required>
+                        <input type="number" step="0.01" class="form-control" name="h_cgpa" id="h_cgpa" placeholder=" " required>
                         <label for="h_cgpa">Highest CGPA</label>
                     </div>
 
